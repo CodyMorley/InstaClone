0458c33788a6db08f71dd153d8283288cd2398b0
Morley <codymorleyinc@gmail.com>
2024-04-21 12:56:11 -0700

    Search View UI
    Initial commit. Added View template.

---
+++ InstaClone/InstaClone/Core/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  InstaClone
+//
+//  Created by Cody Morley on 4/21/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
