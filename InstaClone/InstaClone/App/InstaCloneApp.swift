29f9437cff17a3bcbca0e2b19f42fbb0c9832c57
Morley <codymorleyinc@gmail.com>
2024-04-20 18:11:37 -0700

    Project Skeleton
    Finished Group outline for initial project structure.

---
+++ InstaClone/InstaClone/App/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Cody Morley on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
