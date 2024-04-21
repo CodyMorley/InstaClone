b216afc25190e4b58e8f327294e6b75126f680e9
Morley <codymorleyinc@gmail.com>
2024-04-20 18:49:57 -0700

    Profile pics and stats view
    set up profile pic and stats panel on top of profile ui

--- InstaClone/InstaClone/Core/Profile/ProfileView.swift
+++ InstaClone/InstaClone/Core/Profile/ProfileView.swift
@@ -14,11 +14,51 @@ struct ProfileView: View {
             VStack {
                 HStack {
                     // User Profile Pic + Stats
+                    Image("silvio")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 80, height: 80)
+                        .clipShape(Circle())
+                    
+                    Spacer()
+                    
+                    HStack(spacing: 8) {
+                        VStack {
+                            Text("3")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                            
+                            Text("Posts")
+                                .font(.footnote)
+                        }
+                        .frame(width: 76)
+                        
+                        
+                        VStack {
+                            Text("556")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                            
+                            Text("Followers")
+                                .font(.footnote)
+                        }
+                        .frame(width: 76)
+                        
+                        VStack {
+                            Text("12")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                            Text("Following")
+                                .font(.footnote)
+                        }
+                        .frame(width: 76)
+                    }
                 }
+                .padding(.horizontal)
                 
                 VStack(alignment: .leading, spacing: 4) {
                     // name
-                    Text("Silvio Dante")
+                    Text("Silvio M. Dante")
                         .font(.footnote)
                         .fontWeight(.semibold)
                     // bio
