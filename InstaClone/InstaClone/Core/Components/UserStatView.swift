c38789925b9ca3c94d9b68420fba221b37712bac
Morley <codymorleyinc@gmail.com>
2024-04-21 00:23:55 -0700

    FInished Initial Profile UI
    All main sections of profile view finished.

---
+++ InstaClone/InstaClone/Core/Components/UserStatView.swift
@@ -0,0 +1,29 @@
+//
+//  UserStatView.swift
+//  InstaClone
+//
+//  Created by Cody Morley on 4/20/24.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    let value: Int
+    let title: String
+    
+    var body: some View {
+        VStack {
+            Text("\(value)")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            
+            Text(title)
+                .font(.footnote)
+        }
+        .frame(width: 76)
+    }
+}
+
+#Preview {
+    UserStatView(value: 3, title: "Posts")
+}
--- InstaClone/InstaClone/Core/Profile/ProfileView.swift
+++ InstaClone/InstaClone/Core/Profile/ProfileView.swift
@@ -8,85 +8,89 @@
 import SwiftUI
 
 struct ProfileView: View {
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
+    
     var body: some View {
-        VStack {
-            // header
-            VStack {
-                HStack {
-                    // User Profile Pic + Stats
-                    Image("silvio")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
-                    
-                    Spacer()
-                    
-                    HStack(spacing: 8) {
-                        VStack {
-                            Text("3")
-                                .font(.subheadline)
-                                .fontWeight(.semibold)
+        NavigationStack {
+            ScrollView {
+                // header
+                VStack(spacing: 10) {
+                    HStack {
+                        // User Profile Pic + Stats
+                        Image("silvio")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(value: 3, title: "Posts")
                             
-                            Text("Posts")
-                                .font(.footnote)
-                        }
-                        .frame(width: 76)
-                        
-                        
-                        VStack {
-                            Text("556")
-                                .font(.subheadline)
-                                .fontWeight(.semibold)
+                            UserStatView(value: 556, title: "Followers")
                             
-                            Text("Followers")
-                                .font(.footnote)
+                            UserStatView(value: 12, title: "")
                         }
-                        .frame(width: 76)
-                        
-                        VStack {
-                            Text("12")
-                                .font(.subheadline)
-                                .fontWeight(.semibold)
-                            Text("Following")
-                                .font(.footnote)
-                        }
-                        .frame(width: 76)
+                    }
+                    .padding(.horizontal)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        // name
+                        Text("Silvio M. Dante")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        // bio
+                        Text("Proprietor of The Bada Bing off Route 17")
+                            .font(.footnote)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    
+                    Button {
+                        // edit profile button action
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(.black)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+                    }
+                    
+                    Divider()
+                }
+                
+                // post grid view
+                LazyVGrid(columns: gridItems, spacing: 2) {
+                    ForEach(0...15, id: \.self) { index in
+                        Image("silvio")
+                            .resizable()
+                            .scaledToFill()
                     }
                 }
-                .padding(.horizontal)
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    // name
-                    Text("Silvio M. Dante")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                    // bio
-                    Text("Proprietor of The Bada Bing off Route 17")
-                        .font(.footnote)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                
-                
-                Button {
-                    // edit profile button action
-                } label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundColor(.black)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
-                }
-                
-                Divider()
             }
-            
-            // post grid view
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
         }
     }
 }
