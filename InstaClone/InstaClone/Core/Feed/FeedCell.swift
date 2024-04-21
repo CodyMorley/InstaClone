4d006314bb2d1006970767e7d12b438555c8e69b
Morley <codymorleyinc@gmail.com>
2024-04-21 12:19:42 -0700

    FeedCell Outline
    UI outline for single feed item.

--- InstaClone/InstaClone/Core/Feed/FeedCell.swift
+++ InstaClone/InstaClone/Core/Feed/FeedCell.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct FeedCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                //image and username
+            }
+            
+            // post image
+            
+            HStack {
+                // interactive elements like comment share
+            }
+            
+            // likes counter
+            
+            // caption
+        }
     }
 }
 
