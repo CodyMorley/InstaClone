466f27a24fe8df73b6ad84e487574e89be9ce9d8
Morley <codymorleyinc@gmail.com>
2024-04-21 12:38:39 -0700

    FeedCell
    finished placeholder UI for FeedCell

--- InstaClone/InstaClone/Core/Feed/FeedCell.swift
+++ InstaClone/InstaClone/Core/Feed/FeedCell.swift
@@ -11,18 +11,83 @@ struct FeedCell: View {
     var body: some View {
         VStack {
             HStack {
-                //image and username
+                //user image and username
+                Image("silvio")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                Text("Silvio M. Dante")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
             }
+            .padding(.leading, 8)
             
             // post image
+            Image("bbq")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
             
-            HStack {
-                // interactive elements like comment share
+            HStack(spacing: 16) {
+                // interactive elements like-comment-share
+                Button {
+                    NSLog("Like Post")
+                } label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    NSLog("Comment Post")
+                } label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    NSLog("Share Post")
+                } label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+                
+                Spacer()
             }
+            .padding(.leading, 8)
+            .padding(.top, 4)
+            .foregroundColor(.black)
             
             // likes counter
+            Text("15 likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
             
             // caption
+            HStack {
+                Text("Silvio M. Dante ")
+                    .fontWeight(.semibold) +
+                Text("Saturdays are for the boys.")
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.footnote)
+            .padding(.leading, 10)
+            .padding(.top, 1)
+            
+            // time stamp
+            Text("6h ago")
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+                .foregroundColor(.gray)
         }
     }
 }
