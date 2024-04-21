775b8e55922df2ffebb16e3aea6e77abb745912a
Morley <codymorleyinc@gmail.com>
2024-04-21 13:09:37 -0700

    Search View UI
    placeholder UI for search view finished.

--- InstaClone/InstaClone/Core/Search/SearchView.swift
+++ InstaClone/InstaClone/Core/Search/SearchView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0...15, id: \.self) { user in
-                    Text("Silvio")
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0...15, id: \.self) { user in
+                        HStack {
+                            Image("silvio")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("sillyD")
+                                    .fontWeight(.semibold)
+                                
+                                Text("Silvio M. Dante")
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
                 }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search...")
             }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
