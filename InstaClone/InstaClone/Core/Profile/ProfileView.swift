c49aa6de4ce5cd00f7ea7f3db2192736bb61ae9b
Morley <codymorleyinc@gmail.com>
2024-04-20 18:29:22 -0700

    Edit Profile Button
    Basic view outline added along with label and styling for edit profile button.

--- InstaClone/InstaClone/Core/Profile/ProfileView.swift
+++ InstaClone/InstaClone/Core/Profile/ProfileView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // header
+            VStack {
+                HStack {
+                    // User Profile Pic + Stats
+                }
+                
+                VStack {
+                    // name and bio
+                }
+                
+                
+                Button {
+                    // edit profile button action
+                } label: {
+                    Text("Edit Profile")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 32)
+                        .foregroundColor(.black)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                }
+                
+                Divider()
+            }
+            
+            // post grid view
+        }
     }
 }
 
