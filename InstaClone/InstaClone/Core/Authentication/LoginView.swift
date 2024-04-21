de7e3c6c33acb0a61c3b2608291a25b3bea4c483
Morley <codymorleyinc@gmail.com>
2024-04-21 15:22:54 -0700

    Login View UI
    finished placeholder Login UI

--- InstaClone/InstaClone/Core/Authentication/LoginView.swift
+++ InstaClone/InstaClone/Core/Authentication/LoginView.swift
@@ -8,8 +8,116 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Spacer()
+            
+            VStack {
+                
+                // logo image
+                Image("instagram-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 100)
+                
+                // text fields
+                TextField("Enter your email", text: $email)
+                    .autocapitalization(.none)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+                
+                SecureField("Password", text: $password)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+            }
+            
+            // forgot password button
+            Button {
+                NSLog("Forgot Password Button Tapped.")
+            } label: {
+                Text("Forgot Password?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .padding(.top)
+                    .padding(.trailing, 28)
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            
+            // Log In
+            Button {
+                NSLog("Login Button Tapped.")
+            } label: {
+                Text("Log In")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 360, height: 44)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(8)
+                
+            }
+            .padding(.vertical)
+            
+            
+            HStack {
+                Rectangle()
+                    .frame(width: (UIScreen.main.bounds.width / 2) - 40,
+                           height: 0.5)
+                
+                Text("OR")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.gray)
+                
+                Rectangle()
+                    .frame(width: (UIScreen.main.bounds.width / 2) - 40,
+                           height: 0.5)
+            }
+            .foregroundColor(.gray)
+            
+            // Continue with Facebook Button (inactive in demo)
+            HStack {
+                Image("facebook-f-logo")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                
+                Text("Continue with Facebook")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(.systemBlue))
+            }
+            .padding(.top, 8)
+            .onTapGesture {
+                NSLog("Continue With Facebook")
+            }
+            
+            Spacer()
+            
+            Divider()
+            
+            NavigationLink {
+                Text("Sign Up")
+            } label: {
+                HStack(spacing: 3) {
+                    Text("Don't have an account?")
+                    
+                    Text("Sign Up")
+                        .fontWeight(.semibold)
+                }
+                .font(.footnote)
+            }
+            .padding(.vertical, 16)
+
+        }
     }
 }
 
