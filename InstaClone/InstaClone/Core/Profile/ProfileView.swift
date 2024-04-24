203b74bbd4b0d7f6fcdca7d0e43dba8bc79f5ff9
Morley <codymorleyinc@gmail.com>
2024-04-24 00:43:12 -0700

    ProfileHeaderView
    Extracted a repeated component between profile views

--- InstaClone/InstaClone/Core/Profile/ProfileView.swift
+++ InstaClone/InstaClone/Core/Profile/ProfileView.swift
@@ -18,63 +18,7 @@ struct ProfileView: View {
     var body: some View {
         ScrollView {
             // header
-            VStack(spacing: 10) {
-                HStack {
-                    // User Profile Pic + Stats
-                    Image(user.profileImageURL ?? "")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
-                    
-                    Spacer()
-                    
-                    HStack(spacing: 8) {
-                        UserStatView(value: 3,
-                                     title: "Posts")
-                        
-                        UserStatView(value: 556,
-                                     title: "Followers")
-                        
-                        UserStatView(value: 12,
-                                     title: "Following")
-                    }
-                }
-                .padding(.horizontal)
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    // name
-                    if let full = user.fullName {
-                        Text(full)
-                            .font(.footnote)
-                            .fontWeight(.semibold)
-                    }
-                    // bio
-                    if let bio = user.bio {
-                        Text(bio)
-                            .font(.footnote)
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                
-                
-                Button {
-                    // edit profile button action
-                } label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundColor(.black)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
-                }
-                
-                Divider()
-            }
+            ProfileHeaderView(user: user)
             
             // post grid view
             PostGridView(posts: posts)
