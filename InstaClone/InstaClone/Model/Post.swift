354d00103bdae11c45650252e647f5e25c27be29
Morley <codymorleyinc@gmail.com>
2024-04-23 23:48:31 -0700

    Post Data Model
    Finished inital data model and mock extension for Post objects.

--- InstaClone/InstaClone/Model/Post.swift
+++ InstaClone/InstaClone/Model/Post.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+
+struct Post: Codable, Hashable, Identifiable {
+    let caption: String
+    let id: String
+    let imageURL: String
+    let likes: Int
+    let ownerUUID: String
+    let timestamp: Date
+    var user: User?
+}
+
+
+extension Post {
+    static var mockPosts: [Post] = [
+        .init(caption: "Saturdays are for the boys.",
+              id: UUID().uuidString,
+              imageURL: "bbq",
+              likes: 91,
+              ownerUUID: User.mockUsers[0].id,
+              timestamp: Date.now,
+              user: User.mockUsers[0]),
+        .init(caption: "Dancing with my friend",
+              id: UUID().uuidString,
+              imageURL: "carmela-furio",
+              likes: 44,
+              ownerUUID: User.mockUsers[4].id,
+              timestamp: Date.now - 1000,
+              user: User.mockUsers[4]),
+        .init(caption: "The crew",
+              id: UUID().uuidString,
+              imageURL: "soprano-crew",
+              likes: 102,
+              ownerUUID: User.mockUsers[1].id,
+              timestamp: Date.now - 2000,
+              user: User.mockUsers[1]),
+        .init(caption: "This goombah life is a pain in the neck",
+              id: UUID().uuidString,
+              imageURL: "chris",
+              likes: 1950,
+              ownerUUID: User.mockUsers[2].id,
+              timestamp: Date.now - 3000,
+              user: User.mockUsers[2]),
+        .init(caption: "Photo courtesy of F-Note Records",
+              id: UUID().uuidString,
+              imageURL: "hesh",
+              likes: 9,
+              ownerUUID: User.mockUsers[3].id,
+              timestamp: Date.now - 5000,
+              user: User.mockUsers[3]),
+        .init(caption: "RIP Kemosabe",
+              id: UUID().uuidString,
+              imageURL: "jackie-funeral",
+              likes: 13,
+              ownerUUID: User.mockUsers[0].id,
+              timestamp: Date.now - 6000,
+              user: User.mockUsers[0]),
+        .init(caption: "In here it's 1959",
+              id: UUID().uuidString,
+              imageURL: "tony",
+              likes: 91,
+              ownerUUID: User.mockUsers[1].id,
+              timestamp: Date.now - 10000,
+              user: User.mockUsers[1])
+    ]
+}
