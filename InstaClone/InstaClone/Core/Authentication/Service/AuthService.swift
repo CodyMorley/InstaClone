5f62cbae7f9ec96ea6baf903bcf1eed33f71b1ef
Morley <codymorleyinc@gmail.com>
2024-04-24 16:26:15 -0700

    WIP 1
    Added placeholder functions to implement auth service

--- InstaClone/InstaClone/Core/Authentication/Service/AuthService.swift
+++ InstaClone/InstaClone/Core/Authentication/Service/AuthService.swift
@@ -5,4 +5,31 @@
 //  Created by Cody Morley on 4/24/24.
 //
 
-import Foundation
+import FirebaseAuth
+import SwiftUI
+
+class AuthService {
+    @Published var session: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        self.session = Auth.auth().currentUser
+    }
+    
+    func login(for email: String, password: String) async throws {
+        
+    }
+    
+    func createUser(for email: String, password: String) async throws {
+        
+    }
+    
+    func loadUserData() async throws {
+        
+    }
+    
+    func signOut() {
+        
+    }
+}
