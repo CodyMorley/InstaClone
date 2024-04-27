b7c89fb49d02a12a5acc9bcc6887568dd445e919
Morley <codymorleyinc@gmail.com>
2024-04-27 13:05:04 -0700

    Profile Header Button
    Implemented different button appearance and action handler for profile header based on state of user as logged in or not.

--- InstaClone/InstaClone/Core/Components/ProfileHeaderView.swift
+++ InstaClone/InstaClone/Core/Components/ProfileHeaderView.swift
@@ -53,16 +53,22 @@ struct ProfileHeaderView: View {
             
             
             Button {
-                // edit profile button action
+                if user.isCurrentUser {
+                    NSLog("Show edit profile")
+                } else {
+                    NSLog("Follow")
+                }
             } label: {
-                Text("Edit Profile")
+                Text(user.isCurrentUser ? "Edit Profile" : "Follow")
                     .font(.subheadline)
                     .fontWeight(.semibold)
                     .frame(width: 360, height: 32)
-                    .foregroundColor(.black)
+                    .background(user.isCurrentUser ? .white : Color(.systemBlue))
+                    .foregroundColor(user.isCurrentUser ? .black : .white)
+                    .cornerRadius(6)
                     .overlay(
                         RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.gray, lineWidth: 1)
+                            .stroke(user.isCurrentUser ? .gray : .clear, lineWidth: 1)
                     )
             }
             
