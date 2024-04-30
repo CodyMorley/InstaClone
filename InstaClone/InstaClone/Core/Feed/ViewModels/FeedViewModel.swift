80712df76d980337690d96ddb7b7390b11c1a524
Morley <codymorleyinc@gmail.com>
2024-04-30 12:24:03 -0700

    Feed State
    Feed view model now fetches posts.

---
+++ InstaClone/InstaClone/Core/Feed/ViewModels/FeedViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  FeedViewModel.swift
+//  InstaClone
+//
+//  Created by Cody Morley on 4/30/24.
+//
+
+import Firebase
+import Foundation
+
+class FeedViewModel: ObservableObject {
+    @Published var posts = [Post]()
+    
+    init() {
+        Task { try await fetchPosts() }
+    }
+    
+    func fetchPosts() async throws {
+        let data = try await Firestore.firestore().collection("posts").getDocuments()
+        posts = try data.documents.compactMap({ try $0.data(as: Post.self) })
+    }
+}
